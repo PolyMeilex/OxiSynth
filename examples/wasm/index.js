1375c0bf3efa899b4c4a50028ecd5eb3597b86da
Poly <marynczak.bartlomiej@gmail.com>
2021-03-10 18:07:38 +0100

    Update wasm example

--- examples/wasm/index.js
+++ examples/wasm/index.js
@@ -1,27 +1,48 @@
+function create_btn(label, cb) {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.classList.add("waves-effect");
+  btn.id = label;
+  btn.innerText = label;
+
+  btn.addEventListener("click", cb);
+
+  return btn;
+}
+
 import("./pkg")
   .catch(console.error)
   .then((rust_module) => {
     document.getElementById("start").addEventListener("click", () => {
       let handle = rust_module.beep();
 
-      document.getElementById("C").addEventListener("click", () => {
-        rust_module.noteOn(handle, 60);
-      });
+      const notLoaded = document.getElementById("not-loaded");
+      const loaded = document.getElementById("loaded");
+      const noteBtns = document.getElementById("note-btns");
 
-      document.getElementById("C#").addEventListener("click", () => {
-        rust_module.noteOn(handle, 61);
-      });
+      const labels = [
+        "C",
+        "C#",
+        "D",
+        "D#",
+        "E",
+        "F",
+        "F#",
+        "G",
+        "G#",
+        "A",
+        "A#",
+        "H",
+      ];
 
-      document.getElementById("D").addEventListener("click", () => {
-        rust_module.noteOn(handle, 62);
-      });
+      for (let id = 0; id < 12; id++) {
+        const btn = create_btn(labels[id], () => {
+          rust_module.noteOn(handle, 60 + id);
+        });
+        noteBtns.appendChild(btn);
+      }
 
-      document.getElementById("D#").addEventListener("click", () => {
-        rust_module.noteOn(handle, 63);
-      });
-
-      document.getElementById("E").addEventListener("click", () => {
-        rust_module.noteOn(handle, 64);
-      });
+      notLoaded.style.display = "none";
+      loaded.style.display = "block";
     });
   });
